12b18c4f7b513b21943442bd494525b0cccbb360
Freiregu73 <gustavofreire191@gmail.com>
2026-04-22 21:38:26 -0300

    22_04

---
+++ Aula22_04/src/main/Exercicios.java
@@ -0,0 +1,64 @@
+package main;
+
+import javax.swing.JOptionPane;
+
+public class Exercicios {
+
+    public static String mostrar = "", x = "";
+    public static int v1 = 0, v2 = 0;
+
+    public static void exerc() {
+        JOptionPane.showMessageDialog(null, "Escreva um algoritmo que leia dois vetores de 10 posições e faça a multiplicação dos elementos de mesmo índice, colocando o resultado em um terceiro vetor. \n"
+                + "Mostre o vetor resultante. \n");
+
+        int[] valor1 = new int[10];
+        int[] valor2 = new int[10];
+        int[] resp = new int[10];
+
+        JOptionPane.showMessageDialog(null, "Digite os valores do primeiro vetor");
+        for (int c = 0; c < 10; c++) {
+            x = JOptionPane.showInputDialog(null, "Digite o " + (c + 1) + " valor");
+            v1 = Integer.parseInt(x);
+            valor1[c] = v1;
+        }
+
+        JOptionPane.showMessageDialog(null, "Digite os valores do segundo vetor");
+        for (int c = 0; c < 10; c++) {
+            x = JOptionPane.showInputDialog(null, "Digite o " + (c + 1) + " valor");
+            v2 = Integer.parseInt(x);
+            valor2[c] = v2;
+        }
+
+        for (int c = 0; c < 10; c++) {
+            resp[c] = valor1[c] * valor2[c];
+            mostrar = mostrar + resp[c] + '\n';
+        }
+
+        JOptionPane.showMessageDialog(null, "A multiplicação dos vetores é \n" + mostrar);
+
+    }
+
+    public static void exerc2() {
+        JOptionPane.showMessageDialog(null, "Escreva um algoritmo que leia e mostre um vetor de 20 elementos inteiros.\n"
+                + " a seguir, conte quantos valores pares existem no vetor. ");
+        
+        int par =0;
+        int [] vetor = new int[20];
+        JOptionPane.showMessageDialog(null, "Digite os valores do vetor");
+        for (int c = 0; c < 20; c++) {
+            x = JOptionPane.showInputDialog(null, "Digite o " + (c + 1) + " valor");
+            v1 = Integer.parseInt(x);
+            vetor[c] = v1;
+        }
+        
+        for (int c = 0; c < 20; c++){
+            if(vetor[c]%2 ==0){
+                par++;
+            }
+        }
+        
+        JOptionPane.showMessageDialog(null, "Existem " + par + " números pares no vetor");
+        
+    }
+
+}
---
+++ Aula22_04/src/main/Main.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Laboratorio-Info
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+//        int [] valor = new int[5];
+//                      //0 1 2 3 4 5
+//       int [] valor2 = {1,2,3,4,5,6};
+//       double [] valor3 = {1,2.89,3.67,4.56,5,6};
+//       String [] resp ={"a" , "b", "c", "d"};
+//       char [] resp2= {'a', 'b','c'};
+//       
+//       valor[0] = 10;
+//       valor[1] = 10;
+//       valor[2] = 10;
+//       valor[3] = 10;
+//       valor[4] = 10;
+//       String mostra= "";
+//               
+//       for(int c = 0; c <= 4 ; c++){
+//           String x = JOptionPane.showInputDialog(null, "Digite o valor " + (c+1));
+//           valor[c] = Integer.parseInt(x);
+//           mostra = mostra + valor[c] + " | ";
+//       }
+//       
+//       
+//         JOptionPane.showMessageDialog(null, mostra);  
+         
+         //EXERCICIOS
+         
+         //Exercicios.exerc();
+         Exercicios.exerc2();
+         
+    }
+    
+}
