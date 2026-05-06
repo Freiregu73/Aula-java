10b3f915bb0ab5f6100cddb95f28dd1ff0f44825
Freiregu73 <gustavofreire191@gmail.com>
2026-05-06 20:30:20 -0300

    aula 06/05

---
+++ exemploCalc_06_05/src/Model/Funcoes_DAO.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import static View.Inicio_GUI.VALOR1_TXT;
+import static View.Inicio_GUI.VALOR2_TXT;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Laboratorio-Info
+ */
+public class Funcoes_DAO {
+    
+    public static int val1 =0, val2=0;
+    
+    public static void soma(){
+        val1 = Integer.parseInt(VALOR1_TXT.getText());
+        val2 = Integer.parseInt(VALOR2_TXT.getText());
+        JOptionPane.showMessageDialog(null, val1+val2);
+    }
+    
+    public static void subtracao(){
+        val1 = Integer.parseInt(VALOR1_TXT.getText());
+        val2 = Integer.parseInt(VALOR2_TXT.getText());
+        JOptionPane.showMessageDialog(null, val1-val2);
+    }
+    
+    public static void multiplicacao(){
+        val1 = Integer.parseInt(VALOR1_TXT.getText());
+        val2 = Integer.parseInt(VALOR2_TXT.getText());
+        JOptionPane.showMessageDialog(null, val1*val2);
+    }
+    
+    public static void divisao(){
+        val1 = Integer.parseInt(VALOR1_TXT.getText());
+        val2 = Integer.parseInt(VALOR2_TXT.getText());
+        JOptionPane.showMessageDialog(null, val1/val2);
+    }
+    
+    public static void limpar(){
+        VALOR1_TXT.setText("");
+        VALOR2_TXT.setText("");
+    }
+}
