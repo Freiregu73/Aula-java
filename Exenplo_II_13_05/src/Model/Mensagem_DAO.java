b3003b7a53b106186adae2ea83cbb03655eff4cc
Freiregu73 <gustavofreire191@gmail.com>
2026-05-13 20:59:02 -0300

    13/05

---
+++ Exenplo_II_13_05/src/Model/Mensagem_DAO.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Laboratorio-Info
+ */
+public class Mensagem_DAO {
+    
+    public static void chama(){
+        JOptionPane.showMessageDialog(null, "Cadastro de Cliente");
+    }
+    
+    public static void sair(){
+        System.exit(0);
+    }
+    
+    
+}
